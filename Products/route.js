f327270642bf0a11ecb4b2d36c0c293ebf246e96
Abdulrahman Khaled Adam <abdulrahmankhadamm@gmail.com>
2025-04-28 11:51:02 +0300

    Finished product routes protection

--- Products/route.js
+++ Products/route.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const controller = require("./controller");
+const auth = require("../Users/controller");
 const router = express.Router();
 
-router.route("/").get(controller.getAllProducts).post(controller.createProduct);
-router.route("/:id").get(controller.getProduct).delete(controller.deleteProduct).put(controller.editProduct);
+router.route("/").get(controller.getAllProducts).post(auth.protect, controller.createProduct);
+router
+  .route("/:id")
+  .get(controller.getProduct)
+  .delete(auth.protect, controller.deleteProduct)
+  .put(auth.protect, controller.editProduct);
 
 module.exports = router;
